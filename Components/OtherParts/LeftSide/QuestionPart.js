1d4f8075e4c5f1b2cc0f3a8e46b7e32766ce348d
Raihan <rhnchdri@gmail.com>
2023-02-11 19:18:03 +0600

    img updated

--- Components/OtherParts/LeftSide/QuestionPart.js
+++ Components/OtherParts/LeftSide/QuestionPart.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import img1 from '../../assets/img1.PNG';
-import img2 from '../../assets/img2.PNG';
-import img3 from '../../assets/img3.PNG';
-import img4 from '../../assets/img4.PNG';
-import img5 from '../../assets/img5.PNG';
+import img1 from '../../assets/img1.jpg';
+import img2 from '../../assets//img2.jpg';
+import img3 from '../../assets/img3.jpg';
+import img4 from '../../assets/img4.jpg';
+import img5 from '../../assets/img5.jpg';
 import { BsFillCaretDownFill } from "react-icons/bs";
 
 const QuestionPart = () => {
